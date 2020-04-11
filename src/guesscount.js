f107532bbbb2c873120f5e8045e46e3027acc4ef
Matti Benoliel <benolielmatti@hotmail.fr>
2020-04-11 19:29:15 +0200

    proptypes

--- src/guesscount.js
+++ src/guesscount.js
@@ -1,7 +1,11 @@
-import React from 'react'
-
-import './GuessCount.css'
+import React from 'react';
+import PropTypes from 'prop-types'
+import './GuessCount.css';
 
 const GuessCount = () => <div className="guesses" />
+
+GuessCount.propTypes = {
+  guesses: PropTypes.number.isRequired,
+}
 
 export default GuessCount
