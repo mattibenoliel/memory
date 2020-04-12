adbccc3affa80b73e936472b2f5a7d953329315d
Matti Benoliel <benolielmatti@hotmail.fr>
2020-04-12 11:53:32 +0200

    part3 cours React - exercice fini

--- src/guesscount.js
+++ src/guesscount.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import './GuessCount.css';
 
-const GuessCount = () => <div className="guesses" />
+const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>
 
 GuessCount.propTypes = {
   guesses: PropTypes.number.isRequired,
